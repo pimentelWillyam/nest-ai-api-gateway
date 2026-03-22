87cd6ee68915955ac79c19ad7951e5d26cf94571
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-03-22 07:35:55 -0300

    feat: adding class approach to main

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const configService = app.get(ConfigService)
-  const port = configService.get<number>('port') ?? 3000
-  await app.listen(port)
-  console.log(`Server is running on port ${port}`)
+export class Application {
+  private static getValidationPipe() {
+    return new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  }
+
+  static async execute() {
+    const app = await NestFactory.create(AppModule)
+    app.useGlobalPipes(Application.getValidationPipe())
+    const configService = app.get(ConfigService)
+    const port = configService.get<number>('port') ?? 3000
+    await app.listen(port)
+    console.log(`Server is running on port ${port}`)
+  }
 }
-void bootstrap()
+
+void Application.execute()
