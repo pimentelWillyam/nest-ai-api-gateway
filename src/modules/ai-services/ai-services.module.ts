53c28de0c667557a183e4407d061a2adaf875aae
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 20:53:41 -0300

    feat: adding ai services module

--- src/modules/ai-services/ai-services.module.ts
+++ src/modules/ai-services/ai-services.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AiServicesService } from './ai-services.service';
-import { AiServicesController } from './ai-services.controller';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AiServicesController } from './ai-services.controller'
+import { AiServicesService } from './ai-services.service'
+import { AiService } from './ai-service.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AiService])],
   controllers: [AiServicesController],
   providers: [AiServicesService],
 })
