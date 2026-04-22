677a0a6e6a67039ef9c5935aafb780199198cb96
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 20:35:52 -0300

    feat: adding ai services controller

--- src/modules/ai-services/ai-services.controller.ts
+++ src/modules/ai-services/ai-services.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AiServicesService } from './ai-services.service';
-import { CreateAiServiceDto } from './dto/create-ai-service.dto';
-import { UpdateAiServiceDto } from './dto/update-ai-service.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common'
+import { AiServicesService } from './ai-services.service'
+import { CreateAiServiceDto } from './dto/create-ai-service.dto'
+import { UpdateAiServiceDto } from './dto/update-ai-service.dto'
 
 @Controller('ai-services')
 export class AiServicesController {
@@ -9,26 +20,30 @@ export class AiServicesController {
 
   @Post()
   create(@Body() createAiServiceDto: CreateAiServiceDto) {
-    return this.aiServicesService.create(createAiServiceDto);
+    return this.aiServicesService.create(createAiServiceDto)
   }
 
   @Get()
   findAll() {
-    return this.aiServicesService.findAll();
+    return this.aiServicesService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.aiServicesService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.aiServicesService.findOne(id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAiServiceDto: UpdateAiServiceDto) {
-    return this.aiServicesService.update(+id, updateAiServiceDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateAiServiceDto: UpdateAiServiceDto,
+  ) {
+    return this.aiServicesService.update(id, updateAiServiceDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.aiServicesService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.aiServicesService.remove(id)
   }
 }
