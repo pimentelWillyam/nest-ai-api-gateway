21a7d3257c3b880abd0e25271d4826de7a259089
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 20:35:08 -0300

    feat: adding ai service entity

---
+++ src/modules/ai-services/ai-service.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+} from 'typeorm'
+
+@Entity('ai_services')
+export class AiService {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string
+
+  @Column({ type: 'varchar', length: 255 })
+  model!: string
+
+  @Index({ unique: true })
+  @Column({ type: 'varchar', length: 255 })
+  slug!: string
+
+  @Index({ unique: true })
+  @Column({ type: 'varchar', length: 255 })
+  url!: string
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt!: Date
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt!: Date
+}
