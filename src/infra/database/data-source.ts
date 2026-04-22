0bbf54e11f277f5caed4af13629f2b8138126be3
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 20:30:00 -0300

    feat: adding typeorm datasource

---
+++ src/infra/database/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
+
+dotenv.config({
+  path: path.resolve(process.cwd(), envFile),
+});
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/infra/database/migrations/*.ts'],
+  synchronize: false,
+});
