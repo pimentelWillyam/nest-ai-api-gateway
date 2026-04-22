b9d0efa7f3cc6ac8484d8a065ea27851fa16ee43
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 20:54:25 -0300

    feat: adding create ai service dto

--- src/modules/ai-services/dto/create-ai-service.dto.ts
+++ src/modules/ai-services/dto/create-ai-service.dto.ts
@@ -1,1 +1,12 @@
-export class CreateAiServiceDto {}
+import { IsString, IsUrl, MinLength } from 'class-validator'
+
+export class CreateAiServiceDto {
+  @IsString()
+  model!: string
+
+  @IsString()
+  slug!: string
+
+  @IsUrl()
+  url!: string
+}
