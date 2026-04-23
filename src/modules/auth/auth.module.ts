6e9d7fd538a4c47eae73eedbe99906c2056577a5
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 21:28:27 -0300

    feat: adding auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { User } from '../user/user.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
