225c7bc8efab6d909a40e56840d341fa80147b12
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-04-22 21:49:00 -0300

    chore: adding first migration

---
+++ src/infra/database/migrations/1776895269309-init.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1776895269309 implements MigrationInterface {
+    name = 'Init1776895269309'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying(255) NOT NULL, "login" character varying(100) NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_97672ac88f789774dd47f7c8be" ON "users" ("email") `);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_2d443082eccd5198f95f2a36e2" ON "users" ("login") `);
+        await queryRunner.query(`CREATE TABLE "ai_services" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "model" character varying(255) NOT NULL, "slug" character varying(255) NOT NULL, "url" character varying(255) NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_de00bfbc8a43dd17175c65a185c" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_22ad0af925a9a6ef0bc1ce7e11" ON "ai_services" ("slug") `);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_9bcd564609b8c62f5c91a6f4ba" ON "ai_services" ("url") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_9bcd564609b8c62f5c91a6f4ba"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_22ad0af925a9a6ef0bc1ce7e11"`);
+        await queryRunner.query(`DROP TABLE "ai_services"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_2d443082eccd5198f95f2a36e2"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_97672ac88f789774dd47f7c8be"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
