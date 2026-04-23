21b683f579e9bb3136f7a41cdbc1692376df8a02
Willyam Henrique <willyampimenteldev@gmail.com>
2026-04-23 15:28:10 -0300

    style: formatting

--- src/infra/database/data-source.ts
+++ src/infra/database/data-source.ts
@@ -1,12 +1,12 @@
-import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+import { DataSource } from 'typeorm'
+import * as dotenv from 'dotenv'
+import * as path from 'path'
 
-const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
+const envFile = `.env.${process.env.NODE_ENV || 'dev'}`
 
 dotenv.config({
   path: path.resolve(process.cwd(), envFile),
-});
+})
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -14,4 +14,4 @@ export const AppDataSource = new DataSource({
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/infra/database/migrations/*.ts'],
   synchronize: false,
-});
+})
