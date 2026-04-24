f19773b36b5999eb2eb2848ddd79e0e6affe4a66
Willyam Henrique <willyampimenteldev@gmail.com>
2026-04-24 14:08:39 -0300

    feat: adding user entity at ai services module

--- src/modules/ai-services/ai-services.module.ts
+++ src/modules/ai-services/ai-services.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AiServicesController } from './ai-services.controller'
 import { AiServicesService } from './ai-services.service'
 import { AiService } from './ai-service.entity'
+import { User } from '../user/user.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AiService])],
+  imports: [TypeOrmModule.forFeature([AiService, User])],
   controllers: [AiServicesController],
   providers: [AiServicesService],
 })
