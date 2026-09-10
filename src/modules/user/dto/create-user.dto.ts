9837f5a3ec7464db74d23c5b1e662cfeac87a055
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-09-10 15:28:05 -0300

    refactor: adding role to create user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -10,4 +10,7 @@ export class CreateUserDto {
   @IsString()
   @MinLength(6, { message: 'The password must be at least 6 characters long' })
   password!: string
+
+  @IsString()
+  role!: 'admin' | 'user'
 }
