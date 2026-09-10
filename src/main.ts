d815c133a55568996384df9b014e93e554a91e0f
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-09-10 15:27:20 -0300

    style: making global pipes styled

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { RoutesPrinterHelper } from './helpers/routes-printer.helper'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
 
   const configService = app.get(ConfigService)
   const routesPrinter = app.get(RoutesPrinterHelper)
